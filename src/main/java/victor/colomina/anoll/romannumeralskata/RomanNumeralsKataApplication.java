7bf18936a827c789b7c20cf1c4947e69fdd8eb49
VictorColominaAnoll <victorcolominanoll@gmail.com>
2021-06-09 09:46:14 +0200

    first commit

---
+++ src/main/java/victor/colomina/anoll/romannumeralskata/RomanNumeralsKataApplication.java
@@ -0,0 +1,13 @@
+package victor.colomina.anoll.romannumeralskata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanNumeralsKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomanNumeralsKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/victor/colomina/anoll/romannumeralskata/RomanNumeralsKataApplicationTests.java
@@ -0,0 +1,13 @@
+package victor.colomina.anoll.romannumeralskata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanNumeralsKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
